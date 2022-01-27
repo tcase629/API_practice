6b78e0aba0d5ed1dcc128d60704485bde12a921a
Tyler Case <tcase629@yahoo.com>
2022-01-26 22:04:50 -0700

    App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Container, Row, Col } from 'react-bootstrap';
 
-function App() {
+const App = () => {
+  const [tweets, setTweets] = useState([])
+
+  useEffect( () => {
+    axios.get('/api/tweets')
+      .then( res => {
+        setTweets(res.data)
+      })
+      .catch( err => console.log(err))
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Row>
+        <Col xs={12} md={8}>
+          <h1>Tweets</h1>
+          {
+            tweets.map( tweet =>
+              <div key={tweet.id}>
+                <img src={tweet.user.profile_image_url} alt=''/>
+                <h3>{tweet.user.name}</h3>
+                <p>{tweet.text}</p>
+              </div>
+            )
+          }
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
